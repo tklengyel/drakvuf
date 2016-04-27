tklengyel/drakvuf

commit ae593b8525007e039c467762ee5ab235cc60e728

Check return values in more places

src/plugins/exmon/exmon.cpp
@@ -310,18 +310,21 @@ exmon::exmon(drakvuf_t drakvuf, const void *config, output_format_t output) {
     this->trap.cb = cb;
     this->trap.data = (void*)this;
     this->format = output;
-    this->offsets = (size_t*)g_malloc0(__OFFSET_MAX*sizeof(size_t));
 
     int i;
     for(i=0;i<__OFFSET_MAX;i++) {
-        drakvuf_get_struct_member_rva(rekall_profile, offset_names[i][0], offset_names[i][1],
-                                      &this->offsets[i]);
+        if(VMI_FAILURE == drakvuf_get_struct_member_rva(rekall_profile, offset_names[i][0], offset_names[i][1],
+                                                       &this->offsets[i]) )
+            throw -1;
     }
 
-    drakvuf_get_struct_size(rekall_profile, "_KTRAP_FRAME", &this->ktrap_frame_size);
+    if(VMI_FAILURE == drakvuf_get_struct_size(rekall_profile, "_KTRAP_FRAME", &this->ktrap_frame_size))
+        throw -1;
 
     if ( !drakvuf_add_trap(drakvuf,&this->trap) )
         throw -1;
+
+    this->offsets = (size_t*)g_malloc0(__OFFSET_MAX*sizeof(size_t));
 }
 
 exmon::~exmon() {

src/libdrakvuf/vmi.c
@@ -757,7 +757,10 @@ void drakvuf_loop(drakvuf_t drakvuf) {
     interrupt_event.callback = int3_cb;
     interrupt_event.data = drakvuf;
 
-    vmi_register_event(drakvuf->vmi, &interrupt_event);
+    if(VMI_FAILURE == vmi_register_event(drakvuf->vmi, &interrupt_event)) {
+        fprintf(stderr, "Failed to register interrupt event\n");
+        return;
+    }
 
     int rc = xc_altp2m_switch_to_view(drakvuf->xen->xc, drakvuf->domID, drakvuf->altp2m_idx);
     if ( rc < 0 ) {

commit 191ddac63d0dfe9b1889efc0922c24df182c14ac

Fixing crash introduced by ae593b8

src/plugins/exmon/exmon.cpp
@@ -310,21 +310,21 @@ exmon::exmon(drakvuf_t drakvuf, const void *config, output_format_t output) {
     this->trap.cb = cb;
     this->trap.data = (void*)this;
     this->format = output;
+    this->offsets = (addr_t*)g_malloc0(__OFFSET_MAX*sizeof(addr_t));
 
     int i;
-    for(i=0;i<__OFFSET_MAX;i++) {
-        if(VMI_FAILURE == drakvuf_get_struct_member_rva(rekall_profile, offset_names[i][0], offset_names[i][1],
-                                                       &this->offsets[i]) )
-            throw -1;
+    for(i=0;i<__OFFSET_MAX;i++)
+        drakvuf_get_struct_member_rva(rekall_profile, offset_names[i][0], offset_names[i][1], &this->offsets[i]);
+
+    if(VMI_FAILURE == drakvuf_get_struct_size(rekall_profile, "_KTRAP_FRAME", &this->ktrap_frame_size)) {
+        free(this->offsets);
+        throw -1;
     }
 
-    if(VMI_FAILURE == drakvuf_get_struct_size(rekall_profile, "_KTRAP_FRAME", &this->ktrap_frame_size))
+    if ( !drakvuf_add_trap(drakvuf,&this->trap) ) {
+        free(this->offsets);
         throw -1;
-
-    if ( !drakvuf_add_trap(drakvuf,&this->trap) )
-        throw -1;
-
-    this->offsets = (size_t*)g_malloc0(__OFFSET_MAX*sizeof(size_t));
+    }
 }
 
 exmon::~exmon() {

src/plugins/plugins.cpp
@@ -167,7 +167,7 @@ bool drakvuf_plugins::start(const drakvuf_plugin_t plugin_id,
             break;
         };
         } catch (int e) {
-            fprintf(stderr, "Plugin %i startup failed!\n", plugin_id);
+            fprintf(stderr, "Plugin %s startup failed!\n", drakvuf_plugin_names[plugin_id]);
             return 0;
         }
 