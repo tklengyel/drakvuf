ae795e5d26a47e961e997e9b005d34c74d004170
Dmitry Isaykin <disaykin@ptsecurity.com>
2018-02-16 19:43:47 +0300

    Add key-value output format for regmon plugin

--- src/plugins/regmon/regmon.cpp
+++ src/plugins/regmon/regmon.cpp
@@ -138,6 +138,15 @@ static event_response_t log_reg_hook( drakvuf_t drakvuf, drakvuf_trap_info_t* in
                     printf("\n");
                     break;
 
+                case OUTPUT_KV:
+                    printf("regmon Time=" FORMAT_TIMEVAL ",PID=%d,PPID=%d,ProcessName=\"%s\",Method=%s,Key=\"%s\"",
+                           UNPACK_TIMEVAL(info->timestamp), info->proc_data.pid, info->proc_data.ppid, info->proc_data.name,
+                           info->trap->name, key_path);
+                    if (with_value_name)
+                        printf(",ValueName=\"%s\"", value_name);
+                    printf("\n");
+                    break;
+
                 default:
                 case OUTPUT_DEFAULT:
                     printf("[REGMON] TIME:" FORMAT_TIMEVAL " VCPU:%" PRIu32 " CR3:0x%" PRIx64 ", EPROCESS:0x%" PRIx64 ", PID:%d, PPID:%d, \"%s\" %s:%" PRIi64 " %s:%s",
@@ -216,6 +225,12 @@ static event_response_t log_reg_objattr_hook(drakvuf_t drakvuf, drakvuf_trap_inf
             case OUTPUT_CSV:
                 printf("regmon," FORMAT_TIMEVAL ",%" PRIu32 ",0x%" PRIx64 ",\"%s\",%" PRIi64",%s,%s%s%s\n",
                        UNPACK_TIMEVAL(info->timestamp), info->vcpu, info->regs->cr3, info->proc_data.name, info->proc_data.userid, syscall_name, key_root, key_sep, key_name );
+                break;
+
+            case OUTPUT_KV:
+                printf("regmon Time=" FORMAT_TIMEVAL ",PID=%d,PPID=%d,ProcessName=\"%s\",Method=%s,Key=\"%s%s%s\"\n",
+                       UNPACK_TIMEVAL(info->timestamp), info->proc_data.pid, info->proc_data.ppid, info->proc_data.name,
+                       info->trap->name, key_root, key_sep, key_name );
                 break;
 
             default:
