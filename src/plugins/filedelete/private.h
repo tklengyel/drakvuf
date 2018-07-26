33cedd483ec4b97ebb83e5d2c276a07195116061
Dmitry Isaykin <disaykin@ptsecurity.com>
2018-07-26 16:10:52 +0300

    Filedelete plugin refactored and fixed.
    * Print about detected deleted or changed files in
    filedelete_use_injector mode.
    * Some code duplication removed.
    * Memory leaks fixed.
    * Static counter usage eliminated.
    * Don't mix C++ and C style memory management.

--- src/plugins/filedelete/private.h
+++ src/plugins/filedelete/private.h
@@ -102,8 +102,7 @@
  *                                                                         *
  ***************************************************************************/
 
-#ifndef FILEDELETE_PRIVATE_H
-#define FILEDELETE_PRIVATE_H
+#pragma once
 
 #define FILE_DISPOSITION_INFORMATION 13
 
@@ -129,14 +128,3 @@ enum offset
 };
 
 extern const char* offset_names[__OFFSET_MAX][2];
-
-void register_trap( drakvuf_t drakvuf, const char* rekall_profile, const char* syscall_name,
-                    drakvuf_trap_t* trap,
-                    event_response_t(*hook_cb)( drakvuf_t drakvuf, drakvuf_trap_info_t* info ) );
-
-unicode_string_t* get_file_name(filedelete* f, drakvuf_t drakvuf,
-                                vmi_instance_t vmi,
-                                drakvuf_trap_info_t* info, addr_t handle,
-                                addr_t* out_file, addr_t* out_filetype);
-
-#endif // FILEDELETE_PRIVATE_H
