e22303c3004ba9298610b42cd86601cf1a0c4e34
Sergey Palitsyn <spalitsyn@ptsecurity.com>
2018-08-23 11:34:26 +0300

    Added delaymon for printing values of NtDelayExecution(...) functionn calls

---
+++ src/plugins/delaymon/delaymon.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "plugins/plugins.h"
+#include "plugins/private.h"
+
+class delaymon: public plugin
+{
+public:
+    page_mode_t pm;
+    output_format_t format;
+    win_ver_t winver;
+
+    drakvuf_trap_t trap =
+    {
+        .breakpoint.lookup_type = LOOKUP_PID,
+        .breakpoint.pid = 4,
+        .breakpoint.addr_type = ADDR_RVA,
+        .breakpoint.module = "ntoskrnl.exe",
+        .type = BREAKPOINT,
+        .data = (void*)this
+    };
+
+    delaymon(drakvuf_t drakvuf, const void* config, output_format_t output);
+    ~delaymon();
+};
