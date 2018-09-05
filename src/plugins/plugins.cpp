ef4618fccc258dd6ef53c1e907973260f8ec5c11
skvl <valor@list.ru>
2018-09-05 18:00:20 +0300

    BSOD monitor (#461)
    * Add BSOD detection plugin
    
    * Print BugCheck code and parameters. Setup hook for 'KeBugCheck'.
    
    On 'x86' the 'KeBugCheck' doesn't call 'KeBugCheckEx'.
    
    * Hook 'KeBugCheck2' on x86 to avoid bouble breaks on single page
    
    On x86 'KeBugcheck' and 'KeBugCheckEx' both call 'KeBugCheck2'.

--- src/plugins/plugins.cpp
+++ src/plugins/plugins.cpp
@@ -117,6 +117,7 @@
 #include "socketmon/socketmon.h"
 #include "regmon/regmon.h"
 #include "procmon/procmon.h"
+#include "bsodmon/bsodmon.h"
 
 drakvuf_plugins::drakvuf_plugins(const drakvuf_t drakvuf, output_format_t output, os_t os)
 {
@@ -211,6 +212,11 @@ int drakvuf_plugins::start(const drakvuf_plugin_t plugin_id,
 #ifdef ENABLE_PLUGIN_PROCMON
                 case PLUGIN_PROCMON:
                     this->plugins[plugin_id] = new procmon(this->drakvuf, config, this->output);
+                    break;
+#endif
+#ifdef ENABLE_PLUGIN_BSODMON
+                case PLUGIN_BSODMON:
+                    this->plugins[plugin_id] = new bsodmon(this->drakvuf, config, this->output);
                     break;
 #endif
                 default:
