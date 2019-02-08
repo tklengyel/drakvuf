2523a7c430a9f6ec473b31f1dadbbfe3cf4ed67b
Matt Leinhos <mleinhos@users.noreply.github.com>
2019-02-08 17:13:56 -0600

    JSON-formatted output (#543)
    * Fixed stack corruption in poolmon plugin.
    
    * Initial JSON output support. Not yet tested by parser.
    
    * Fixes to JSON message output.
    
    * Intermediate changes: fixes to JSON messages.
    
    * Improved JSON output; still in-progress.
    
    * Further improvements to JSON output.
    
    Can process about 1.5 million events before drakvuf halts here:
    
    0x00007fe3928b6154 in __GI___libc_write (fd=0x2, buf=0x7fe393a2f5d4, nbytes=0xb) at ../sysdeps/unix/sysv/linux/write.c:27
    27	../sysdeps/unix/sysv/linux/write.c: No such file or directory.
    195	            errprint("create_new_entry failed\n");
    190
    191	        dbprint(VMI_DEBUG_MEMCACHE, "--MEMORY cache set 0x%"PRIx64"\n", paddr);
    192
    193	        entry = create_new_entry(vmi, paddr, vmi->page_size);
    194	        if (!entry) {
    195	            errprint("create_new_entry failed\n");
    196	            return 0;
    197	        }
    
    * Updated submodules...
    
    * libvmi now in-sync with upstream's submodule
    
    * JSON: Fixed a couple straggling syntax errors from merge.
    
    * Added async version of run_drakvuf.py
    
    Minor formatting fixes too.
    
    * JSON output: cleaned up code per code-review.
    
    * Updated formatting for acceptance.
    
    Attempt #1.
    
    * Fixed formatting for PR.
    
    Attempt #2.
    
    * Fixed formatting for PR.
    
    Try #3
    
    * Fixed formatting for PR.
    
    Try #4.
    
    * Updated to use external JSON library.
    
    * Added submodule, fixed formatting.
    
    * JSON: Removed extraneous call, improved error output
    
    * Minor fix.
    
    * Removed dependence on new JSON library.
    
    Updated JSON utility function to use existing dependency (json-c).
    libdrakvuf reverted to exclusively C.
    
    * Cleanup after reworking of JSON escape function.
    
    Function surrounds string with double-quotes for the caller.
    Callers updated so result type is gchar* rather than char*.
    
    * Removed stale #includes.
    
    * Fixed compilation warning.
    
    * Correct a few JSON output messages.
    
    * [procmon] print ProcessHandle of NtOpenProcess (#550)
    
    * socketmon: fixed swallowing last character in ip address string (#551)
    
    socketmon: decrease code duplication
    
    * last_error fix (#553)
    
    * Initial JSON output support. Not yet tested by parser.
    
    * Fixes to JSON message output.
    
    * Intermediate changes: fixes to JSON messages.
    
    * Improved JSON output; still in-progress.
    
    * Further improvements to JSON output.
    
    Can process about 1.5 million events before drakvuf halts here:
    
    0x00007fe3928b6154 in __GI___libc_write (fd=0x2, buf=0x7fe393a2f5d4, nbytes=0xb) at ../sysdeps/unix/sysv/linux/write.c:27
    27	../sysdeps/unix/sysv/linux/write.c: No such file or directory.
    195	            errprint("create_new_entry failed\n");
    190
    191	        dbprint(VMI_DEBUG_MEMCACHE, "--MEMORY cache set 0x%"PRIx64"\n", paddr);
    192
    193	        entry = create_new_entry(vmi, paddr, vmi->page_size);
    194	        if (!entry) {
    195	            errprint("create_new_entry failed\n");
    196	            return 0;
    197	        }
    
    * JSON: Fixed a couple straggling syntax errors from merge.
    
    * Added async version of run_drakvuf.py
    
    Minor formatting fixes too.
    
    * JSON output: cleaned up code per code-review.
    
    * Updated formatting for acceptance.
    
    Attempt #1.
    
    * Fixed formatting for PR.
    
    Attempt #2.
    
    * Fixed formatting for PR.
    
    Try #3
    
    * Fixed formatting for PR.
    
    Try #4.
    
    * Updated to use external JSON library.
    
    * Added submodule, fixed formatting.
    
    * JSON: Removed extraneous call, improved error output
    
    * Minor fix.
    
    * Removed dependence on new JSON library.
    
    Updated JSON utility function to use existing dependency (json-c).
    libdrakvuf reverted to exclusively C.
    
    * Cleanup after reworking of JSON escape function.
    
    Function surrounds string with double-quotes for the caller.
    Callers updated so result type is gchar* rather than char*.
    
    * Removed stale #includes.
    
    * Fixed compilation warning.
    
    * Correct a few JSON output messages.
    
    * Updated stale variable usage in procmon.

--- src/plugins/cpuidmon/cpuidmon.cpp
+++ src/plugins/cpuidmon/cpuidmon.cpp
@@ -125,6 +125,7 @@ event_response_t cpuid_cb(drakvuf_t drakvuf, drakvuf_trap_info_t* info)
 {
 
     cpuidmon* s = (cpuidmon*)info->trap->data;
+    gchar* escaped_pname = NULL;
 
     switch (s->format)
     {
@@ -140,6 +141,34 @@ event_response_t cpuid_cb(drakvuf_t drakvuf, drakvuf_trap_info_t* info)
                    UNPACK_TIMEVAL(info->timestamp), info->proc_data.pid, info->proc_data.ppid, info->proc_data.name,
                    info->cpuid->leaf, info->cpuid->subleaf,
                    info->regs->rax, info->regs->rbx, info->regs->rcx, info->regs->rdx);
+            break;
+
+        case OUTPUT_JSON:
+            escaped_pname = drakvuf_escape_str(info->proc_data.name);
+            printf( "{"
+                    "\"Plugin\" : \"cpuidmon\","
+                    "\"TimeStamp\" :" "\"" FORMAT_TIMEVAL "\","
+                    "\"VCPU\": %" PRIu32 ","
+                    "\"CR3\": %" PRIu64 ","
+                    "\"ProcessName\": %s,"
+                    "\"UserName\": \"%s\","
+                    "\"UserId\": %" PRIu64 ","
+                    "\"PID\" : %d,"
+                    "\"PPID\": %d,"
+                    "\"Leaf\": %" PRIu32 ","
+                    "\"Subleaf\": %" PRIu32 ","
+                    "\"RAX\": %" PRIu64 ","
+                    "\"RBX\": %" PRIu64 ","
+                    "\"RCX\": %" PRIu64 ","
+                    "\"RDX\": %" PRIu64 ""
+                    "}\n",
+                    UNPACK_TIMEVAL(info->timestamp),
+                    info->vcpu, info->regs->cr3, escaped_pname,
+                    USERIDSTR(drakvuf), info->proc_data.userid,
+                    info->proc_data.pid, info->proc_data.ppid,
+                    info->cpuid->leaf, info->cpuid->subleaf,
+                    info->regs->rax, info->regs->rbx, info->regs->rcx, info->regs->rdx);
+            g_free(escaped_pname);
             break;
 
         default:
--- src/plugins/syscalls/syscalls.cpp
+++ src/plugins/syscalls/syscalls.cpp
@@ -126,6 +126,28 @@ static event_response_t linux_cb(drakvuf_t drakvuf, drakvuf_trap_info_t* info)
             printf("syscall Time=" FORMAT_TIMEVAL ",PID=%d,PPID=%d,ProcessName=\"%s\",Method=%s\n",
                    UNPACK_TIMEVAL(t), info->proc_data.pid, info->proc_data.ppid, info->proc_data.name, info->trap->name);
             break;
+        case OUTPUT_JSON:
+            // Place single EOL at end of JSON doc to simplify parsing on other end
+            printf( "{"
+                    "\"Plugin\" : \"syscall\","
+                    "\"TimeStamp\" :" "\"" FORMAT_TIMEVAL "\","
+                    "\"VCPU\": %" PRIu32 ","
+                    "\"CR3\": %" PRIu64 ","
+                    "\"ProcessName\": \"%s\","
+                    "\"UserName\": \"%s\","
+                    "\"UserId\": %" PRIu64 ","
+                    "\"PID\" : %d,"
+                    "\"PPID\": %d,"
+                    "\"Module\": \"%s\","
+                    "\"Method\": \"%s\""
+                    "}\n",
+                    UNPACK_TIMEVAL(t),
+                    info->vcpu, info->regs->cr3, info->proc_data.name,
+                    USERIDSTR(drakvuf), info->proc_data.userid,
+                    info->proc_data.pid, info->proc_data.ppid,
+                    info->trap->breakpoint.module, info->trap->name);
+            break;
+
         default:
         case OUTPUT_DEFAULT:
             printf("[SYSCALL] TIME:" FORMAT_TIMEVAL " VCPU:%" PRIu32 " CR3:0x%" PRIx64 ",\"%s\" %s:%" PRIi64" %s!%s\n",
@@ -166,6 +188,8 @@ static char* extract_utf8_string(drakvuf_t drakvuf, drakvuf_trap_info_t* info, c
 
 static void print_header(output_format_t format, drakvuf_t drakvuf, const drakvuf_trap_info_t* info)
 {
+    gchar* escaped_pname = NULL;
+
     switch (format)
     {
         case OUTPUT_CSV:
@@ -178,8 +202,32 @@ static void print_header(output_format_t format, drakvuf_t drakvuf, const drakvu
                    UNPACK_TIMEVAL(info->timestamp), info->proc_data.pid, info->proc_data.ppid, info->proc_data.name,
                    info->trap->name);
             break;
-        default:
+        case OUTPUT_JSON:
+            // print_footer() puts single EOL at end of JSON doc to simplify parsing on other end
+            escaped_pname = drakvuf_escape_str(info->proc_data.name);
+            printf( "{"
+                    "\"Plugin\" : \"syscall\","
+                    "\"TimeStamp\" :" "\"" FORMAT_TIMEVAL "\","
+                    "\"VCPU\": %" PRIu32 ","
+                    "\"CR3\": %" PRIu64 ","
+                    "\"ProcessName\": %s,"
+                    "\"UserName\": \"%s\","
+                    "\"UserId\": %" PRIu64 ","
+                    "\"PID\" : %d,"
+                    "\"PPID\": %d,"
+                    "\"Module\": \"%s\","
+                    "\"Method\": \"%s\","
+                    "\"Args\": [ ",
+                    UNPACK_TIMEVAL(info->timestamp),
+                    info->vcpu, info->regs->cr3, escaped_pname,
+                    USERIDSTR(drakvuf), info->proc_data.userid,
+                    info->proc_data.pid, info->proc_data.ppid,
+                    info->trap->breakpoint.module, info->trap->name);
+            g_free(escaped_pname);
+            break;
+
         case OUTPUT_DEFAULT:
+        default:
             printf("[SYSCALL] TIME:" FORMAT_TIMEVAL " VCPU:%" PRIu32 " CR3:0x%" PRIx64 ",\"%s\" %s:%" PRIi64" %s!%s",
                    UNPACK_TIMEVAL(info->timestamp), info->vcpu, info->regs->cr3, info->proc_data.name,
                    USERIDSTR(drakvuf), info->proc_data.userid,
@@ -196,6 +244,7 @@ static void print_nargs(output_format_t format, uint32_t nargs)
             printf(",%" PRIu32, nargs);
             break;
         case OUTPUT_KV:
+        case OUTPUT_JSON:
             break;
         default:
         case OUTPUT_DEFAULT:
@@ -235,6 +284,29 @@ static void print_kv_arg(syscalls* s, drakvuf_t drakvuf, drakvuf_trap_info_t* in
         printf(",%s=0x%" PRIx64, arg.name, static_cast<uint64_t>(val));
 }
 
+
+static void print_json_arg(syscalls* s, drakvuf_t drakvuf, drakvuf_trap_info_t* info, const win_syscall_t* wsc, size_t i, addr_t val, const char* str)
+{
+    const win_arg_t& arg = wsc->args[i];
+
+    if ( str )
+    {
+        gchar* escaped = drakvuf_escape_str(str);
+        printf("{\"%s\" : %s}", arg.name, escaped);
+        g_free(escaped);
+    }
+    else
+    {
+        if ( 4 == s->reg_size )
+            printf("{\"%s\" :%"  PRIu32 "}", arg.name, static_cast<uint32_t>(val));
+        else
+            printf("{\"%s\" :%" PRIu64 "}", arg.name, static_cast<uint64_t>(val));
+    }
+
+    if (i < wsc->num_args-1)
+        printf(",");
+}
+
 static void print_default_arg(syscalls* s, drakvuf_t drakvuf, drakvuf_trap_info_t* info, const win_arg_t& arg, addr_t val, const char* str)
 {
     printf("\t%s %s %s: ", win_arg_direction_names[arg.dir], win_type_names[arg.type], arg.name);
@@ -271,6 +343,9 @@ static void print_args(syscalls* s, drakvuf_t drakvuf, drakvuf_trap_info_t* info
             case OUTPUT_KV:
                 print_kv_arg(s, drakvuf, info, wsc->args[i], val, str);
                 break;
+            case OUTPUT_JSON:
+                print_json_arg(s, drakvuf, info, wsc, i, val, str);
+                break;
             default:
             case OUTPUT_DEFAULT:
                 print_default_arg(s, drakvuf, info, wsc->args[i], val, str);
@@ -290,6 +365,10 @@ static void print_footer(output_format_t format, uint32_t nargs)
             break;
         case OUTPUT_KV:
             printf("\n");
+            break;
+        case OUTPUT_JSON:
+            // close JSON args array and document
+            printf("] }\n");
             break;
         default:
         case OUTPUT_DEFAULT:
