730a424513965036a209176232fc6666df79b14b
sifurov <sifurov@gmail.com>
2019-03-25 19:16:22 +0300

    Switch few exists plugin to use plugin_ex (#599)

--- src/plugins/envmon/private.h
+++ src/plugins/envmon/private.h
@@ -106,7 +106,7 @@
 #define ENVMON_PRIVATE_H
 
 // from Windows secext.h
-typedef enum
+enum extended_name_format
 {
     NameUnknown          = 0,
     NameFullyQualifiedDN = 1,
@@ -120,10 +120,10 @@ typedef enum
     NameDnsDomain        = 12,
     NameGivenName        = 13,
     NameSurname          = 14
-} extended_name_format_t;
+};
 
 // from Windows sysinfoapi.h
-typedef enum _COMPUTER_NAME_FORMAT
+enum computer_name_format
 {
     ComputerNameNetBIOS,
     ComputerNameDnsHostname,
@@ -134,6 +134,100 @@ typedef enum _COMPUTER_NAME_FORMAT
     ComputerNamePhysicalDnsDomain,
     ComputerNamePhysicalDnsFullyQualified,
     ComputerNameMax
-} computer_name_format_t;
+};
+
+enum family_types_format
+{
+    AF_UNSPEC   = 0,
+    AF_INET     = 2,
+    AF_INET6    = 23
+};
+
+enum flags_types_format
+{
+    GAA_FLAG_SKIP_UNICAST                   = 0x0001,
+    GAA_FLAG_SKIP_ANYCAST                   = 0x0002,
+    GAA_FLAG_SKIP_MULTICAST                 = 0x0004,
+    GAA_FLAG_SKIP_DNS_SERVER                = 0x0008,
+    GAA_FLAG_INCLUDE_PREFIX                 = 0x0010,
+    GAA_FLAG_SKIP_FRIENDLY_NAME             = 0x0020,
+    GAA_FLAG_INCLUDE_WINS_INFO              = 0x0040,
+    GAA_FLAG_INCLUDE_GATEWAYS               = 0x0080,
+    GAA_FLAG_INCLUDE_ALL_INTERFACES         = 0x0100,
+    GAA_FLAG_INCLUDE_ALL_COMPARTMENTS       = 0x0200,
+    GAA_FLAG_INCLUDE_TUNNEL_BINDINGORDER    = 0x0400
+};
+
+// emum network_types_name
+// {
+//     WNNC_NET_MSNET       = 0x00010000,
+//     WNNC_NET_SMB         = 0x00020000,
+//     WNNC_NET_NETWARE     = 0x00030000,
+//     WNNC_NET_VINES       = 0x00040000,
+//     WNNC_NET_10NET       = 0x00050000,
+//     WNNC_NET_LOCUS       = 0x00060000,
+//     WNNC_NET_SUN_PC_NFS  = 0x00070000,
+//     WNNC_NET_LANSTEP     = 0x00080000,
+//     WNNC_NET_9TILES      = 0x00090000,
+//     WNNC_NET_LANTASTIC   = 0x000A0000,
+//     WNNC_NET_AS400       = 0x000B0000,
+//     WNNC_NET_FTP_NFS     = 0x000C0000,
+//     WNNC_NET_PATHWORKS   = 0x000D0000,
+//     WNNC_NET_LIFENET     = 0x000E0000,
+//     WNNC_NET_POWERLAN    = 0x000F0000,
+//     WNNC_NET_BWNFS       = 0x00100000,
+//     WNNC_NET_COGENT      = 0x00110000,
+//     WNNC_NET_FARALLON    = 0x00120000,
+//     WNNC_NET_APPLETALK   = 0x00130000,
+//     WNNC_NET_INTERGRAPH  = 0x00140000,
+//     WNNC_NET_SYMFONET    = 0x00150000,
+//     WNNC_NET_CLEARCASE   = 0x00160000,
+//     WNNC_NET_FRONTIER    = 0x00170000,
+//     WNNC_NET_BMC         = 0x00180000,
+//     WNNC_NET_DCE         = 0x00190000,
+//     WNNC_NET_AVID        = 0x001A0000,
+//     WNNC_NET_DOCUSPACE   = 0x001B0000,
+//     WNNC_NET_MANGOSOFT   = 0x001C0000,
+//     WNNC_NET_SERNET      = 0x001D0000,
+//     WNNC_NET_RIVERFRONT1 = 0X001E0000,
+//     WNNC_NET_RIVERFRONT2 = 0x001F0000,
+//     WNNC_NET_DECORB      = 0x00200000,
+//     WNNC_NET_PROTSTOR    = 0x00210000,
+//     WNNC_NET_FJ_REDIR    = 0x00220000,
+//     WNNC_NET_DISTINCT    = 0x00230000,
+//     WNNC_NET_TWINS       = 0x00240000,
+//     WNNC_NET_RDR2SAMPLE  = 0x00250000,
+//     WNNC_NET_CSC         = 0x00260000,
+//     WNNC_NET_3IN1        = 0x00270000,
+//     WNNC_NET_EXTENDNET   = 0x00290000,
+//     WNNC_NET_STAC        = 0x002A0000,
+//     WNNC_NET_FOXBAT      = 0x002B0000,
+//     WNNC_NET_YAHOO       = 0x002C0000,
+//     WNNC_NET_EXIFS       = 0x002D0000,
+//     WNNC_NET_DAV         = 0x002E0000,
+//     WNNC_NET_KNOWARE     = 0x002F0000,
+//     WNNC_NET_OBJECT_DIRE = 0x00300000,
+//     WNNC_NET_MASFAX      = 0x00310000,
+//     WNNC_NET_HOB_NFS     = 0x00320000,
+//     WNNC_NET_SHIVA       = 0x00330000,
+//     WNNC_NET_IBMAL       = 0x00340000,
+//     WNNC_NET_LOCK        = 0x00350000,
+//     WNNC_NET_TERMSRV     = 0x00360000,
+//     WNNC_NET_SRT         = 0x00370000,
+//     WNNC_NET_QUINCY      = 0x00380000,
+//     WNNC_NET_OPENAFS     = 0x00390000,
+//     WNNC_NET_AVID1       = 0X003A0000,
+//     WNNC_NET_DFS         = 0x003B0000,
+//     WNNC_NET_KWNP        = 0x003C0000,
+//     WNNC_NET_ZENWORKS    = 0x003D0000,
+//     WNNC_NET_DRIVEONWEB  = 0x003E0000,
+//     WNNC_NET_VMWARE      = 0x003F0000,
+//     WNNC_NET_RSFX        = 0x00400000,
+//     WNNC_NET_MFILES      = 0x00410000,
+//     WNNC_NET_MS_NFS      = 0x00420000,
+//     WNNC_NET_GOOGLE      = 0x00430000,
+
+//     WNNC_CRED_MANAGER    = 0xFFFF0000,
+// };
 
 #endif
