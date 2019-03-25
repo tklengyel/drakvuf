87464d7137adc8c38574317e6cb6c9339c394c79
Sergey Kovalev <valor@list.ru>
2019-03-25 18:03:05 +0300

    Add interface for process enumeration

--- src/libdrakvuf/win.c
+++ src/libdrakvuf/win.c
@@ -468,6 +468,7 @@ bool set_os_windows(drakvuf_t drakvuf)
     drakvuf->osi.get_registry_keyhandle_path = win_reg_keyhandle_path;
     drakvuf->osi.get_filename_from_handle = win_get_filename_from_handle;
     drakvuf->osi.get_function_argument = win_get_function_argument;
+    drakvuf->osi.enumerate_processes = win_enumerate_processes;
     drakvuf->osi.enumerate_processes_with_module = win_enumerate_processes_with_module;
     drakvuf->osi.is_crashreporter = win_is_crashreporter;
 
