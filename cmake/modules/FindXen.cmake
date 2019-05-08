0c749a601c8d68e323b445c6a5d7c70749c5466b
Sergey Kovalev <valor@list.ru>
2019-05-08 12:08:34 +0300

    Fix Xen find module

--- cmake/modules/FindXen.cmake
+++ cmake/modules/FindXen.cmake
@@ -11,26 +11,29 @@ find_path(Xen_INCLUDE_DIR
     NAMES xenctrl.h libxl_utils.h)
 
 find_library(Xenctrl_LIBRARY
-             NAMES libxenctrl.so)
-
-find_library(Xentoollog_LIBRARY
-             NAMES libxentoollog.so)
-
-find_library(XL_LIBRARY
-             NAMES libxlutil.so)
+             NAMES xenctrl)
 
 find_library(XenLight_LIBRARY
-             NAMES libxenlight.so)
+             NAMES xenlight)
+
+find_library(Xentoollog_LIBRARY
+             NAMES xentoollog)
+
+find_library(Xlutil_LIBRARY
+             NAMES xlutil)
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(Xen
     DEFAULT_MSG
-    Xen_INCLUDE_DIR Xenctrl_LIBRARY Xentoollog_LIBRARY XL_LIBRARY)
+    Xen_INCLUDE_DIR Xenctrl_LIBRARY XenLight_LIBRARY Xlutil_LIBRARY)
 
 if (Xen_FOUND)
     set(Xen_INCLUDE_DIRS ${Xen_INCLUDE_DIR})
-    set(Xenctrl_LIBRARIES ${Xenctrl_LIBRARY} ${Xentoollog_LIBRARY} ${XL_LIBRARY} ${XenLight_LIBRARY})
+    set(Xen_LIBRARIES ${Xenctrl_LIBRARY} ${XenLight_LIBRARY} ${Xlutil_LIBRARY})
+    if (Xentoollog_LIBRARY)
+        set(Xen_LIBRARIES ${Xen_LIBRARIES} ${Xentoollog_LIBRARY})
+    endif()
 endif ()
 
 mark_as_advanced(Xen_INCLUDE_DIR)
