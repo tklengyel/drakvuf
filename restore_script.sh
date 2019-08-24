100f7056800a3be8dffb678820b15bbd3add2d16
aj <the-elves@bitbucket.org>
2019-08-25 00:42:02 +0530

    restore_script updated

--- restore_script.sh
+++ restore_script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -x
-if [[ `sudo lvscan` == *$OS_FUZZ_XEN_DRIVE* ]]; then
-		sudo lvremove $OS_FUZZ_XEN_DRIVE -y;
+if [[ `sudo lvscan` == *$OS_FUZZ_DOMAIN_VOLUME* ]]; then
+		sudo lvremove $OS_FUZZ_DOMAIN_VOLUME -y;
 fi
-sudo lvcreate -s -L 10G -n $OS_FUZZ_XEN_DRIVE $OS_FUZZ_XEN_DRIVE_SAVED 
+sudo lvcreate -s -L 10G -n $OS_FUZZ_DOMAIN_VOLUME $OS_FUZZ_DOMAIN_SAVED_VOLUME 
 sudo xl restore $OS_FUZZ_XEN_SAV_FILE
-remmina $OS_FUZZ_REMMINA_FILE &
 set +x
