b0a1f5756be7a705e7958a07b62b62e8831ed229
Tamas K Lengyel <tamas@tklengyel.com>
2019-10-27 12:28:54 -0600

    Initialize poolmon variables

--- src/plugins/poolmon/poolmon.cpp
+++ src/plugins/poolmon/poolmon.cpp
@@ -141,8 +141,8 @@ static event_response_t cb(drakvuf_t drakvuf, drakvuf_trap_info_t* info)
 {
     poolmon* p = (poolmon*)info->trap->data;
     page_mode_t pm = drakvuf_get_page_mode(drakvuf);
-    reg_t pool_type;
-    reg_t size;
+    reg_t pool_type = 0;
+    reg_t size = 0;
     char tag[5] = { [0 ... 4] = '\0' };
     struct pooltag* s = NULL;
     const char* pool_type_str;
