6f093c83f45cb92a8b06c66b141d465a57dc61e8
Michał Leszczyński <michal.leszczynski@cert.pl>
2019-11-02 01:25:46 +0100

    package build: allow to specify version manually

--- package/build.sh
+++ package/build.sh
@@ -9,7 +9,7 @@ mkdir -p package/log
 
 if [ ! -f "package/cache/xen-intermediate-$XEN_HASH.tar.gz" ]
 then
-    echo Building Xen intermediate...
+    echo Building Xen intermediate $XEN_HASH...
     docker build -f package/Dockerfile-xen -t xen-intermediate . 2>&1 >package/log/xen-build.log
     if [ $? -ne 0 ]; then echo Xen intermediate image build failed, build log tail below ; tail -n 200 package/log/xen-build.log ; exit 1 ; fi
     echo Removing old Xen intermediate image...
@@ -18,11 +18,11 @@ then
     docker save xen-intermediate | gzip -c > "package/cache/xen-intermediate-$XEN_HASH.tar.gz"
     if [ $? -ne 0 ]; then echo Failed to save Xen intermediate image ; rm package/cache/xen-intermediate-*.tar.gz ; exit 1 ; fi
 else
-    echo Loading cached Xen intermediate...
+    echo Loading cached Xen intermediate $XEN_HASH...
     docker load < "package/cache/xen-intermediate-$XEN_HASH.tar.gz"
     if [ $? -ne 0 ]; then echo Failed to load Xen intermediate image ; exit 1 ; fi
 fi
 
 echo Building final image...
-docker build -f package/Dockerfile-final -t deb-build . && docker run -v $(pwd)/package/out:/out deb-build
+docker build -f package/Dockerfile-final -t deb-build . && docker run -v $(pwd)/package/out:/out deb-build ./package/mkdeb $@
 if [ $? -ne 0 ]; then echo Failed to build package ; exit 1 ; fi
