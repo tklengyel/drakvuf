78fd646b5ea7f47a732de1c92a4091da43ada8a7
Michał Leszczyński <ml@icedev.pl>
2020-05-01 15:55:08 +0200

    Adjust package build process for Ubuntu 20.04 (#868)

--- package/build.sh
+++ package/build.sh
@@ -2,24 +2,33 @@
 
 # run ./package/build.sh
 
+IMAGE="ubuntu:18.04"
+
+if [ ! -z "$1" ]
+then
+    IMAGE="$1"
+    echo "Overriding base image for build to: $IMAGE"
+    shift
+fi
+
 XEN_HASH=$(git ls-files -s xen | cut -f2 '-d ')
 
 mkdir -p package/cache
 mkdir -p package/log
 
-if [ ! -f "package/cache/xen-intermediate-$XEN_HASH.tar.gz" ]
+if [ ! -f "package/cache/xen-intermediate-$IMAGE-$XEN_HASH.tar.gz" ]
 then
     echo Building Xen intermediate $XEN_HASH...
-    docker build -f package/Dockerfile-xen -t xen-intermediate . 2>&1 >package/log/xen-build.log
+    docker build --build-arg "IMAGE=$IMAGE" -f package/Dockerfile-xen -t xen-intermediate . # 2>&1 >package/log/xen-build.log
     if [ $? -ne 0 ]; then echo Xen intermediate image build failed, build log tail below ; tail -n 200 package/log/xen-build.log ; exit 1 ; fi
     echo Removing old Xen intermediate image...
     rm -f package/cache/xen-intermediate-*.tar.gz
     echo Saving Xen intermediate...
-    docker save xen-intermediate | gzip -c > "package/cache/xen-intermediate-$XEN_HASH.tar.gz"
+    docker save xen-intermediate | gzip -c > "package/cache/xen-intermediate-$IMAGE-$XEN_HASH.tar.gz"
     if [ $? -ne 0 ]; then echo Failed to save Xen intermediate image ; rm package/cache/xen-intermediate-*.tar.gz ; exit 1 ; fi
 else
-    echo Loading cached Xen intermediate $XEN_HASH...
-    docker load < "package/cache/xen-intermediate-$XEN_HASH.tar.gz"
+    echo Loading cached Xen intermediate $IMAGE-$XEN_HASH...
+    docker load < "package/cache/xen-intermediate-$IMAGE-$XEN_HASH.tar.gz"
     if [ $? -ne 0 ]; then echo Failed to load Xen intermediate image ; exit 1 ; fi
 fi
 
