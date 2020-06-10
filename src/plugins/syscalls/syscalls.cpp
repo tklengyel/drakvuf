f2f3f0ca2d65e4bfd07313718dce7986a9765377
Andrey Kurzhonkov <kurzhonkov@bk.ru>
2020-06-10 03:43:37 +0000

    [bugsearch]#2

--- src/plugins/syscalls/syscalls.cpp
+++ src/plugins/syscalls/syscalls.cpp
@@ -288,24 +288,22 @@ void print_syscall(output_format_t format, drakvuf_t drakvuf,
                 {
                     args_walker = ArgsWalker<defarg_t>(drakvuf, info, sc, args_data, s->reg_size, default_arg);
                 }
-//XXX
-//                deffmt::print("syscall", drakvuf, info,
-//                              keyval("Module", fmt::Rstr(module)),
-//                              keyval("VCPU", fmt::Nval(info->vcpu)),
-//                              keyval("CR3", fmt::Xval(info->regs->cr3)),
-//                              keyval("Arguments", args_walker)
-//                             );
+                deffmt::print("syscall", drakvuf, info,
+                              keyval("Module", fmt::Rstr(module)),
+                              keyval("VCPU", fmt::Nval(info->vcpu)),
+                              keyval("CR3", fmt::Xval(info->regs->cr3)),
+                              keyval("Arguments", args_walker)
+                             );
             }
             else
             {
-//XXX
-//                deffmt::print("sysret", drakvuf, info,
-//                              keyval("Module", fmt::Rstr(module)),
-//                              keyval("VCPU", fmt::Nval(info->vcpu)),
-//                              keyval("CR3", fmt::Xval(info->regs->cr3)),
-//                              keyval("Ret", fmt::Nval(ret)),
-//                              keyval("Info", fmt::Rstr(extra_info ?: ""))
-//                             );
+                deffmt::print("sysret", drakvuf, info,
+                              keyval("Module", fmt::Rstr(module)),
+                              keyval("VCPU", fmt::Nval(info->vcpu)),
+                              keyval("CR3", fmt::Xval(info->regs->cr3)),
+                              keyval("Ret", fmt::Nval(ret)),
+                              keyval("Info", fmt::Rstr(extra_info ?: ""))
+                             );
             }
             break;
     }
