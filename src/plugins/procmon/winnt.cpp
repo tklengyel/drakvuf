tklengyel/drakvuf

commit d158bf532867075037785eaf72d1657e1450b314

Coverity fixes (#825)

src/plugins/procmon/winnt.cpp
@@ -127,7 +127,7 @@ std::string stringify_protection_attributes(uint32_t attributes, char sep)
         if (num > 1)
             result.append(1, sep);
 
-        switch (attr)
+        switch (attributes)
         {
             case PAGE_NOACCESS:
                 result.append("PAGE_NOACCESS");

src/libdrakvuf/drakvuf.c
@@ -733,7 +733,7 @@ gchar* drakvuf_escape_str(const char* input)
     }
 
     const char* escaped = json_object_to_json_string(obj);
-    if (NULL == obj)
+    if (NULL == escaped)
     {
         fprintf(stderr, "json_object_to_json_string() failed!\n");
         goto exit;

src/libdrakvuf/json-profile.c
@@ -154,7 +154,7 @@ bool json_lookup_array(
                 PRINT_DEBUG("Failed to find address for symbol %s\n", symbol_subsymbol_array[i][0]);
             }
         }
-        else if ( VMI_FAILURE == vmi_get_struct_member_offset_from_json(drakvuf->vmi, json, symbol_subsymbol_array[i][0], symbol_subsymbol_array[i][1], &rva[i]) )
+        else if ( rva && VMI_FAILURE == vmi_get_struct_member_offset_from_json(drakvuf->vmi, json, symbol_subsymbol_array[i][0], symbol_subsymbol_array[i][1], &rva[i]) )
         {
             errors++;
             PRINT_DEBUG("Failed to find offset for %s:%s\n", symbol_subsymbol_array[i][0], symbol_subsymbol_array[i][1]);

src/libdrakvuf/win-handles.c
@@ -151,10 +151,10 @@ static addr_t drakvuf_get_obj_by_handle_impl(drakvuf_t drakvuf, addr_t process, 
             handle -= i;
             j = handle / ((low_count * HANDLE_MULTIPLIER) / psize);
 
-            if ( VMI_SUCCESS == vmi_read_addr_va(vmi, table_base + j, 0, &table) )
-            {
-                vmi_read_addr_va(vmi, table + i * (drakvuf->sizes[HANDLE_TABLE_ENTRY] / HANDLE_MULTIPLIER), 0, &obj);
-            }
+            if ( VMI_FAILURE == vmi_read_addr_va(vmi, table_base + j, 0, &table) ||
+                 VMI_FAILURE == vmi_read_addr_va(vmi, table + i * (drakvuf->sizes[HANDLE_TABLE_ENTRY] / HANDLE_MULTIPLIER), 0, &obj) )
+                return 0;
+
             break;
         }
         case 2:
@@ -173,9 +173,11 @@ static addr_t drakvuf_get_obj_by_handle_impl(drakvuf_t drakvuf, addr_t process, 
             k = j % (mid_count * psize);
             j = (j-k)/mid_count;
 
-            if ( VMI_SUCCESS == vmi_read_addr_va(vmi, table_base + j, 0, &table) &&
-                 VMI_SUCCESS == vmi_read_addr_va(vmi, table + k, 0, &table2) )
-                vmi_read_addr_va(vmi, table2 + i * drakvuf->sizes[HANDLE_TABLE_ENTRY] / HANDLE_MULTIPLIER, 0, &obj);
+            if ( VMI_FAILURE == vmi_read_addr_va(vmi, table_base + j, 0, &table) ||
+                 VMI_FAILURE == vmi_read_addr_va(vmi, table + k, 0, &table2) ||
+                 VMI_FAILURE == vmi_read_addr_va(vmi, table2 + i * drakvuf->sizes[HANDLE_TABLE_ENTRY] / HANDLE_MULTIPLIER, 0, &obj) )
+                return 0;
+
             break;
         }
     }

src/plugins/filetracer/win_acl.cpp
@@ -454,10 +454,10 @@ std::string parse_sid(const uint8_t buffer[])
     auto sid = reinterpret_cast<const struct SID*>(buffer);
     auto rev = static_cast<int>(sid->Revision);
     uint64_t id_auth = 0;
-    for (auto i = 0; i != sizeof(SID_IDENTIFIER_AUTHORITY); ++i)
+    for (uint64_t i = 0; i != sizeof(SID_IDENTIFIER_AUTHORITY); ++i)
     {
-        auto idx = sizeof(SID_IDENTIFIER_AUTHORITY) - 1 - i;
-        auto offset = i * sizeof(uint8_t);
+        uint64_t idx = sizeof(SID_IDENTIFIER_AUTHORITY) - 1 - i;
+        uint64_t offset = i * sizeof(uint8_t);
         id_auth += sid->IdentifierAuthority[idx] << offset;
     }
 

src/plugins/plugins_ex.h
@@ -128,10 +128,12 @@ std::string FieldToString(const std::map<uint64_t, std::string>& maps, const std
     std::size_t indexes = value.size();
     for (std::size_t i = 0; i < indexes; i++)
     {
-        if (!value[i])
+        uint64_t v = value[i];
+
+        if (!v)
             continue;
 
-        auto it = maps.find(value[i] << i);
+        auto it = maps.find(v << i);
         if (it == maps.end())
             continue;
 

src/plugins/regmon/regmon.cpp
@@ -342,9 +342,7 @@ static unicode_string_t* get_data_as_string( drakvuf_t drakvuf, drakvuf_trap_inf
     std::vector<uint8_t> data_bytes(data_size, 0);
 
     size_t bytes_read = 0;
-    vmi_read(vmi_lg.vmi, &ctx, data_size, data_bytes.data(), &bytes_read);
-
-    if (bytes_read != data_size)
+    if ( VMI_FAILURE == vmi_read(vmi_lg.vmi, &ctx, data_size, data_bytes.data(), &bytes_read) )
     {
         PRINT_DEBUG("[REGMON] Error reading data, expected %zu bytes, but actually read %zu\n", data_size, bytes_read);
         return nullptr;

src/plugins/ssdtmon/ssdtmon.h
@@ -115,7 +115,6 @@ public:
     drakvuf_trap_t ssdtwrite;
     drakvuf_trap_t ssdtwrite2;
 
-    size_t ulongs;
     addr_t kiservicetable;
     uint32_t kiservicelimit;
 

commit fb16700a9188d3e2a13fad29f211ff7721c21012

procmon: fix logical error
The error was introduced in d158bf532867075037785eaf72d1657e1450b314

src/plugins/procmon/winnt.cpp
@@ -127,7 +127,7 @@ std::string stringify_protection_attributes(uint32_t attributes, char sep)
         if (num > 1)
             result.append(1, sep);
 
-        switch (attributes)
+        switch (attr)
         {
             case PAGE_NOACCESS:
                 result.append("PAGE_NOACCESS");