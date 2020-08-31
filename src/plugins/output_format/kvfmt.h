tklengyel/drakvuf

commit c2d8bbe8bfdcb6a5014e7a6cbf31088cd2b7b008

output/kvfmt: fix various format errors (#953)
* kvfmt: fix printing vectors

* kvfmt: not print empty strings

* poolmon: fix type for printer

Co-authored-by: Sergey Kovalev <skovalev@ptsecurity.com>

src/plugins/output_format/kvfmt.h
@@ -178,6 +178,9 @@ struct DataPrinter
     template <class Tv = T>
     static bool print(std::ostream& os, const fmt::Rstr<Tv>& data, char)
     {
+        if (data.value.empty())
+            return false;
+
         os << data.value;
         return true;
     }
@@ -185,6 +188,9 @@ struct DataPrinter
     template <class Tv = T>
     static bool print(std::ostream& os, const fmt::Qstr<Tv>& data, char)
     {
+        if (data.value.empty())
+            return false;
+
         os << '"' << data.value << '"';
         return true;
     }
@@ -218,14 +224,18 @@ struct DataPrinter
             "Unsupported KV printer key type");
 
         auto pos = os.tellp();
-        if (print_data(os, fmt::Rstr(data.first), 0))
+        auto is_print_data = true;
+        if (!is_iterable<Tv>::value)
         {
-            os << '=';
-            if (print_data(os, data.second, ';'))
-            {
-                return true;
-            }
+            if (print_data(os, fmt::Rstr(data.first), 0))
+                os << '=';
+            else
+                is_print_data = false;
         }
+
+        if (is_print_data && print_data(os, data.second, ','))
+            return true;
+
         os.seekp(pos);
         return false;
     }
@@ -251,6 +261,9 @@ struct DataPrinter<T, std::enable_if_t<is_iterable<T>::value, void>>
 {
     static bool print(std::ostream& os, const T& data, char sep)
     {
+        if (data.empty())
+            return false;
+
         bool printed = false;
         for (const auto& v : data)
         {

src/plugins/output_format/common.h
@@ -200,48 +200,35 @@ struct Rstr
 template<class T>
 struct Rstr<T,
            std::enable_if_t<
-           std::is_same_v<T, char*>,
+           std::is_same_v<std::decay_t<T>, std::string>
+           || std::is_same_v<std::decay_t<T>, std::string_view>,
            void>
-           >: ValHolder<const std::remove_pointer_t<T>*>
+           >: ValHolder<T>
 {
-    using const_ptr_t = const std::remove_pointer_t<T>* ;
-
-    Rstr(T v): ValHolder<const_ptr_t>(std::move(v))
-    {
-        if constexpr (std::is_same_v<T, const char*>)
-        {
-            if (ValHolder<const_ptr_t>::value == nullptr)
-                ValHolder<const_ptr_t>::value = "(null)";
-        }
-    }
+    Rstr(T v): ValHolder<T>(std::move(v)) {}
 };
 
 template<class T>
 struct Rstr<T,
            std::enable_if_t<
-           std::is_same_v<T, const char*>
-           || std::is_same_v<std::decay_t<T>, std::string>
-           || std::is_same_v<std::decay_t<T>, std::string_view>,
+           std::is_same_v<T, const char*>,
            void>
-           >: ValHolder<T>
+           >: ValHolder<std::string>
 {
-    Rstr(char* v): Rstr<const T>(v) {}
-
-    Rstr(T v): ValHolder<T>(std::move(v))
+    Rstr(T v): ValHolder<std::string>(std::string())
     {
-        if constexpr (std::is_same_v<T, const char*>)
-        {
-            if (ValHolder<T>::value == nullptr)
-                ValHolder<T>::value = "(null)";
-        }
+        if (v == nullptr)
+            ValHolder<std::string>::value = std::string("(null)");
+        else
+            ValHolder<std::string>::value = std::string(v);
     }
 };
 
 /* format specific quoted string value */
 template<class T>
-struct Qstr: Rstr<T>
+struct Qstr: Rstr<std::string>
 {
-    Qstr(T v): Rstr<T>(std::move(v)) {}
+    Qstr(T v): Rstr<std::string>(std::move(v)) {}
 };
 
 } // namespace fmt

src/plugins/poolmon/poolmon.cpp
@@ -193,7 +193,7 @@ static event_response_t cb(drakvuf_t drakvuf, drakvuf_trap_info_t* info)
     }
 
     auto tuple = std::make_tuple(
-                     keyval("Tag", fmt::Rstr(tag)),
+                     keyval("Tag", fmt::Rstr((const char*)tag)),
                      keyval("Type", fmt::Rstr(pool_type_str)),
                      keyval("Size", fmt::Nval(size)),
                      keyval("Source", source),

commit 30109e8f2cacfa2ddc486a696c50d3e83cfd1f8a

kvfmt: fix format for quoted strings
The key value pair with empty quoted string should be printed.

The error was introduced in c2d8bbe8bfdcb6a5014e7a6cbf31088cd2b7b008

src/plugins/output_format/kvfmt.h
@@ -188,9 +188,6 @@ struct DataPrinter
     template <class Tv = T>
     static bool print(std::ostream& os, const fmt::Qstr<Tv>& data, char)
     {
-        if (data.value.empty())
-            return false;
-
         os << '"' << data.value << '"';
         return true;
     }