337140364cf4e10d5d6b80b7fc448bed4ddc303a
Sergey Kovalev <valor@list.ru>
2020-09-02 16:23:05 +0300

    procmon: fix logical error (#977)
    The error was introduced in d158bf532867075037785eaf72d1657e1450b314
    
    Co-authored-by: Sergey Kovalev <skovalev@ptsecurity.com>

--- src/plugins/procmon/winnt.cpp
+++ src/plugins/procmon/winnt.cpp
@@ -127,7 +127,7 @@ std::string stringify_protection_attributes(uint32_t attributes, char sep)
         if (num > 1)
             result.append(1, sep);
 
-        switch (attributes)
+        switch (attr)
         {
             case PAGE_NOACCESS:
                 result.append("PAGE_NOACCESS");
