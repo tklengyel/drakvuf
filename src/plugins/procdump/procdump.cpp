8ccddb786032c9e69500e64f00bf274d1969a536
Tamas K Lengyel <tamas@tklengyel.com>
2020-09-12 19:59:04 -0400

    Remove redundant assignment in procdump

--- src/plugins/procdump/procdump.cpp
+++ src/plugins/procdump/procdump.cpp
@@ -319,9 +319,9 @@ static enum rtlcopy_status dump_with_rtlcopymemory(drakvuf_t drakvuf,
         uint8_t zeros[VMI_PS_4KB] = {};
         for (uint32_t i = 0; i < ptes_to_dump; ++i)
             ctx->writer->append(zeros, VMI_PS_4KB);
+
         vad->second.idx += ptes_to_dump;
-        skip = max_contigious_range(prototype_ptes, total_number_of_ptes,
-                                    vad->second.idx, ptes_to_dump, ctx->POOL_SIZE_IN_PAGES);
+
         if (0 == ptes_to_dump)
         {
             ctx->vads.erase(vad_start);
