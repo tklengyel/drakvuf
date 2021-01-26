eb3a67d1c88b36f837d7dc342575e911b8541bd7
Sergey Kovalev <valor@list.ru>
2021-01-26 12:58:23 +0300

    syscall: add NtUserCreateDesktop, NtUserCreateDesktopEx, NtGdiBitBlt

--- src/plugins/syscalls/win.h
+++ src/plugins/syscalls/win.h
@@ -1043,6 +1043,18 @@ SYSCALL(NtFsControlFile, NTSTATUS, 10,
         "OutputBuffer", "bcount_opt(OutputBufferLength)", DIR_OUT, PVOID,
         "OutputBufferLength", "", DIR_IN, ULONG,
        );
+SYSCALL(NtGdiBitBlt, INT, 11,
+        "hdcDst", "", DIR_IN, HANDLE,
+        "x", "", DIR_IN, INT,
+        "y", "", DIR_IN, INT,
+        "cx", "", DIR_IN, INT,
+        "cy", "", DIR_IN, INT,
+        "hdcSrc", "", DIR_IN, HANDLE,
+        "xSrc", "", DIR_IN, INT,
+        "ySrc", "", DIR_IN, INT,
+        "rop4", "", DIR_IN, DWORD,
+        "crBackColor", "", DIR_IN, DWORD,
+        "fl", "", DIR_IN, ULONG);
 SYSCALL(NtGetContextThread, NTSTATUS, 2,
         "ThreadHandle", "", DIR_IN, HANDLE,
         "ThreadContext", "", DIR_INOUT, PCONTEXT,
@@ -2426,6 +2438,20 @@ SYSCALL(NtUserCallTwoParam, NTSTATUS, 3,
         "Param1", "", DIR_IN, DWORD,
         "Param2", "", DIR_IN, DWORD,
         "Routine", "", DIR_IN, DWORD);
+SYSCALL(NtUserCreateDesktop, NTSTATUS, 6,
+        "ObjectAttributes", "", DIR_IN, POBJECT_ATTRIBUTES,
+        "lpszDevice", "", DIR_IN, PUNICODE_STRING,
+        "devmode", "", DIR_IN, PVOID,
+        "dwflags", "", DIR_IN, DWORD,
+        "access", "", DIR_IN, ACCESS_MASK,
+        "heapsize", "", DIR_IN, DWORD);
+SYSCALL(NtUserCreateDesktopEx, NTSTATUS, 6,
+        "ObjectAttributes", "", DIR_IN, POBJECT_ATTRIBUTES,
+        "lpszDevice", "", DIR_IN, PUNICODE_STRING,
+        "devmode", "", DIR_IN, PVOID,
+        "dwflags", "", DIR_IN, DWORD,
+        "access", "", DIR_IN, ACCESS_MASK,
+        "heapsize", "", DIR_IN, DWORD);
 SYSCALL(NtUserGetAsyncKeyState, SHORT, 1,
         "Key", "", DIR_IN, INT
        );
@@ -2777,7 +2803,6 @@ SYSCALL(NtGdiBRUSHOBJ_pvGetRbrush, NTSTATUS, 0);
 SYSCALL(NtGdiBRUSHOBJ_ulGetBrushColor, NTSTATUS, 0);
 SYSCALL(NtGdiBeginGdiRendering, NTSTATUS, 0);
 SYSCALL(NtGdiBeginPath, NTSTATUS, 0);
-SYSCALL(NtGdiBitBlt, NTSTATUS, 0);
 SYSCALL(NtGdiCLIPOBJ_bEnum, NTSTATUS, 0);
 SYSCALL(NtGdiCLIPOBJ_cEnumStart, NTSTATUS, 0);
 SYSCALL(NtGdiCLIPOBJ_ppoGetPath, NTSTATUS, 0);
@@ -3531,8 +3556,6 @@ SYSCALL(NtUserCreateAcceleratorTable, NTSTATUS, 0);
 SYSCALL(NtUserCreateActivationObject, NTSTATUS, 0);
 SYSCALL(NtUserCreateCaret, NTSTATUS, 0);
 SYSCALL(NtUserCreateDCompositionHwndTarget, NTSTATUS, 0);
-SYSCALL(NtUserCreateDesktop, NTSTATUS, 0);
-SYSCALL(NtUserCreateDesktopEx, NTSTATUS, 0);
 SYSCALL(NtUserCreateEmptyCursorObject, NTSTATUS, 0);
 SYSCALL(NtUserCreateInputContext, NTSTATUS, 0);
 SYSCALL(NtUserCreateLocalMemHandle, NTSTATUS, 0);
