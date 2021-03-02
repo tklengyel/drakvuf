dc5683d67b02d056ca9075fd900ec14e856b0d85
Klaus Schmidt <klaus.schmidt@fau.de>
2021-03-02 16:57:55 +0100

    Correct misaligned code

--- src/plugins/plugins.h
+++ src/plugins/plugins.h
@@ -150,12 +150,12 @@ struct plugins_options
     const char* clr_profile;            // PLUGIN_MEMDUMP
     const char* mscorwks_profile;       // PLUGIN_MEMDUMP
     std::shared_ptr<std::unordered_map<vmi_pid_t, bool>> terminated_processes; // PLUGIN_PROCDUMP
-    const char* codemon_dump_dir;      // PLUGIN_CODEMON
+    const char* codemon_dump_dir;       // PLUGIN_CODEMON
     const char* codemon_filter_executable;  // PLUGIN_CODEMON
-    bool codemon_log_everything;       // PLUGIN_CODEMON
-    bool codemon_dump_vad;             // PLUGIN_CODEMON
+    bool codemon_log_everything;        // PLUGIN_CODEMON
+    bool codemon_dump_vad;              // PLUGIN_CODEMON
     bool codemon_analyse_system_dll_vad;    // PLUGIN_CODEMON
-    bool codemon_default_benign;       // PLUGIN_CODEMON
+    bool codemon_default_benign;        // PLUGIN_CODEMON
 
 };
 
@@ -250,7 +250,7 @@ static const bool drakvuf_plugin_os_support[__DRAKVUF_PLUGIN_LIST_MAX][VMI_OS_WI
     [PLUGIN_PROCDUMP]     = { [VMI_OS_WINDOWS] = 1, [VMI_OS_LINUX] = 0 },
     [PLUGIN_RPCMON]       = { [VMI_OS_WINDOWS] = 1, [VMI_OS_LINUX] = 0 },
     [PLUGIN_TLSMON]       = { [VMI_OS_WINDOWS] = 1, [VMI_OS_LINUX] = 0 },
-    [PLUGIN_CODEMON]     = { [VMI_OS_WINDOWS] = 1, [VMI_OS_LINUX] = 0 },
+    [PLUGIN_CODEMON]      = { [VMI_OS_WINDOWS] = 1, [VMI_OS_LINUX] = 0 },
 };
 
 class plugin
