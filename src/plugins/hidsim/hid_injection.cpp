tklengyel/drakvuf

commit 15d788cb1dfcc10511a09a4995870593e7ef8ef9

Fix issues in hidsim identified by static code analysis (#1255)
* Fix usage of strdup(...) with unterminated string (CID 350859)

Fix usage of strdup(...) with an unterminated string in  l.25
qmp_connection.cpp, which was recorded as CID 350859.

* Fix resource leaks in get_display_dimensions(...) (CID 350857)

Fix resource leaks in get_display_dimensions(...) of the file
/src/plugins/hidsim/hid_injection.cpp, which was identified as
CID 350857 by coverity.

* Fix unterminated strings in qmp_init_conn() (CID 350854)

Fix issues with unterminated strings in qmp_init_conn() of the file
src/plugins/hidsim/qmp_connection.cpp, which were identified
as CID 350854 by coverity.

* Fix potential buffer overflow (CID 350852)

Fix a potential buffer overflow vulnerability in l. 208 of
get_display_dimensions in file src/plugins/hidsim/hid_injection.cpp,
which was identified as CID 350852 by coverity

* Fix submission of tainted data to usleep (CID 350851)

Fix the submission of tainted data to usleep(...) in l. 615
and 677 of run_template_injection() in src/plugins/hidsim/\
hid_injection.cpp, which was identified as CID 350851 by
coverity.

* Fix calculations with tainted scalar (CID 350855)

Fix calculations with tainted scalar in l. 217 f. in
get_display_dimensions(...) of src/plugins/hidsim/hid_injection.cpp,
which was identified as CID 350855 by coverity.

* Replace rand with mt19937 to fix WEAK_CRYPTO-issues (CID 350860)

Replace rand() with Mersenne Twister implementation (std::mt19937)
to fix DC.WEAK_CRYPTO-issues, which were identified as CID 350860,
350858 and 350853 by coverity.

* Clean-up coding style

src/plugins/hidsim/hid_injection.cpp
@@ -106,6 +106,8 @@
 
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
@@ -114,6 +116,7 @@
 #include <json-c/json.h>
 #include <json-c/json_object.h>
 #include <math.h>
+#include <random>
 
 #include <libdrakvuf/libdrakvuf.h> /* eprint_current_time */
 #include "../private.h" /* PRINT_DEBUG */
@@ -134,20 +137,25 @@
 /* Length of the QMP command buffer */
 #define CMD_BUF_LEN 0x400
 
-/* QAPI's button values */
-#define L_BTN_STR "left"
-#define M_BTN_STR "middle"
-#define R_BTN_STR "right"
-
 /* Throttle injection down to 50 microsecond intervals */
 #define TIME_BIN 50
+#define MAX_SLEEP 10 * 1000000
+
+/*
+ * Replaces the flawed rand()-function of C's standard library with the
+ * Mersenne Twister implementation
+ */
+static std::mt19937 _rand;
 
 typedef struct dimensions
 {
-    int width;      /* display width */
-    int height;     /* display height */
-    float dx;       /* one pixel equivalent on x-axis */
-    float dy;       /* one pixel equivalent on y-axis */
+    /* Max screensize 65536 x 65536 */
+    uint16_t width;      /* Display width */
+    uint16_t height;     /* Display height */
+
+    /* Mapping factors for QMP */
+    float dx;       /* One pixel equivalent on x-axis */
+    float dy;       /* One pixel equivalent on y-axis */
 } dimensions;
 
 /* Keep track of the current cursor position */
@@ -194,7 +202,7 @@ static int get_display_dimensions(qmp_connection* qc, dimensions* dims)
 {
     const char* tmp_path = "/tmp/tmp.ppm";
 
-    /* Dumps screen via QMP-command and stores result at tmp_path */
+    /* Dumps screen via QMP as .ppm-file and stores result at tmp_path */
     if (dump_screen(qc, tmp_path) != 0)
         return -1;
 
@@ -204,28 +212,46 @@ static int get_display_dimensions(qmp_connection* qc, dimensions* dims)
     int matches = 0;
     int ret = 0;
 
+    /* Reads first three bytes of the potential portable pixmap */
+    matches = fscanf(fr, "%2s", ppm_hdr);
+
     /* Checks PPM-header */
-    matches = fscanf(fr, "%s", ppm_hdr);
-
     if (matches != 1 || strcmp(ppm_hdr, PPM_MAGIC_NUMBER) != 0)
+    {
+        fprintf(stderr, "[HIDSIM]: Error reading .ppm-files's magic number\n");
+        fclose(fr);
         ret = -1;
+    }
 
-    /* Reads actual dimensions */
-    matches = fscanf(fr, "%d%d", &(dims->width), &(dims->height));
+    /* Retrieves screen dimensions by reading screen dump  */
+    uint32_t width = 0, height = 0;
+    matches = fscanf(fr, "%" SCNd32 "%" SCNd32, &width, &height);
+
+    if (matches != 2 )
+    {
+        fprintf(stderr, "[HIDSIM]: Error reading .ppm-files metadata\n");
+        fclose(fr);
+        ret = -1;
+    }
+
+    /* Sanitizes read values; no need for a screen size larger than 65k */
+    dims->width = (uint16_t)(width > UINT16_MAX ? UINT16_MAX : width);
+    dims->height = (uint16_t)(height > UINT16_MAX ? UINT16_MAX : height);
+
+    /* Clean up */
+    fclose(fr);
+
+    /* Removes temp file generated by the screendump-command */
+    if (remove(tmp_path) != 0)
+        fprintf(stderr, "[HIDSIM]: Error deleting screen dump %s\n", tmp_path);
 
     /* Calculates pixel mapping */
     dims->dx = calculate_pixel_unit(dims->width);
     dims->dy = calculate_pixel_unit(dims->height);
 
-    if (matches != 2 || dims->width < 0 || dims->height < 0)
-        ret = -1;
-
-    /* Clean up */
-    if (remove(tmp_path) != 0)
-        fprintf(stderr, "[HIDSIM]: Error deleting screen dump %s\n", tmp_path);
-
     PRINT_DEBUG("[HIDSIM] Screen dimension: %d x %d\n",
         dims->width, dims->height);
+
     return ret;
 }
 
@@ -393,7 +419,7 @@ double hypot(double a, double b)
 /* Returns a random number n, while a <= n < b */
 int rand_approx_uniform(int a, int b)
 {
-    return a + rand() % (b - a + 1);
+    return a +_rand() % (b - a + 1);
 }
 
 /*
@@ -424,8 +450,8 @@ double gaussian_rand (double mean, double sigma)
     do
     {
         /* Scale it to [-1, 1] by calculating -low + x * (high - low) */
-        u = -1 + ((double)rand() / RAND_MAX) * 2;
-        v = -1 + ((double)rand() / RAND_MAX) * 2;
+        u = -1 + ((double)_rand() / RAND_MAX) * 2;
+        v = -1 + ((double)_rand() / RAND_MAX) * 2;
         w = u * u + v * v;
     }
     /* Avoids division by 0 */
@@ -542,11 +568,7 @@ static int run_random_injection(qmp_connection* qc, sig_atomic_t* has_to_stop)
     int sleep = 0;
     int time_frame = 0;
 
-    /* Seed RNG */
-    struct timeval t;
-    gettimeofday(&t, NULL);
-    srand(t.tv_sec);
-    int s = rand()%512;
+    int s = _rand()%512;
 
     /* Loops, until stopped */
     while (!*has_to_stop)
@@ -605,14 +627,24 @@ static int run_template_injection(qmp_connection* qc, FILE* f,
 
     size_t nr = 0;
     long int sleep = 0;
+    long int sleep_sanitized = 0;
     bool was_last = true;
 
     /* Reads first event */
     nr = fread(&ie_cur, sizeof(ie_cur), 1, f);
 
+    if (nr != 1)
+    {
+        fprintf(stderr, "[HIDSIM] Error reading first input event");
+        return 1;
+    }
+
     /* Waits until first event should be injected */
     sleep = ie_cur.time.tv_sec * 1000000 * ie_cur.time.tv_usec;
-    usleep(sleep);
+
+    /* Sanitizes potentially tainted data */
+    sleep_sanitized = sleep > MAX_SLEEP ? MAX_SLEEP : sleep;
+    usleep(sleep_sanitized);
 
     while (!*has_to_stop)
     {
@@ -638,7 +670,7 @@ static int run_template_injection(qmp_connection* qc, FILE* f,
             /* Resets time for the repeat */
             timerclear(&ie_cur.time);
             /* Ensures injection of pending events */
-            sleep = TIME_BIN +1;
+            sleep = TIME_BIN + 1;
         }
         else
         {
@@ -674,7 +706,11 @@ static int run_template_injection(qmp_connection* qc, FILE* f,
         /* Omits sleeping after injection of last event */
         if (!was_last)
         {
-            usleep(sleep);
+            /* Sanitizes potentially tainted data */
+            sleep_sanitized = sleep > MAX_SLEEP ? MAX_SLEEP : sleep;
+            usleep(sleep_sanitized);
+
+            /* Advances input event to process */
             ie_cur = ie_next;
         }
     }

src/plugins/hidsim/qmp_connection.cpp
@@ -111,9 +111,9 @@
 #include "qmp_connection.h"
 
 /* Checks a result string, it it matches a success response */
-int qmp_check_result_str(const char* s)
+int qmp_check_result_str(const char* s, size_t n)
 {
-    if (strcmp(s, QMP_SUCCESS) != 0)
+    if (strncmp(s, QMP_SUCCESS, n) != 0)
         return -1;
 
     return 0;
@@ -171,6 +171,7 @@ int qmp_init_conn(qmp_connection* qc,  const char* path)
 
     if (ret == -1)
     {
+        qc->buf[BUF_SIZE - 1] = '\0'; /* Ensures termination */
         fprintf(stderr, "Fetching QMP greeting banner failed - %s\n", qc->buf);
         return -1;
     }
@@ -196,8 +197,9 @@ int qmp_init_conn(qmp_connection* qc,  const char* path)
         return -1;
     }
 
-    if (qmp_check_result_str(qc->buf) != 0)
+    if (qmp_check_result_str(qc->buf, BUF_SIZE) != 0)
     {
+        qc->buf[BUF_SIZE - 1] = '\0'; /* Ensures termination */
         fprintf(stderr, "Entering command mode failed, msg: %s\n", qc->buf);
         return -1;
     }
@@ -252,7 +254,7 @@ int qmp_communicate(qmp_connection* qc, const char* in, char** out)
         return ret;
     }
     if (out)
-        *out = strdup(qc->buf);
+        *out = strndup(qc->buf, BUF_SIZE);
 
     memset(qc->buf, 0, sizeof(qc->buf));
 

src/plugins/hidsim/qmp_connection.h
@@ -124,7 +124,6 @@ typedef struct qmp_connection
 {
     int fd;
     struct sockaddr_un sa;
-    json_object* capabilities;
     char buf[BUF_SIZE];
     bool is_connected;
 } qmp_connection;

commit e32be125ebfbfae66b920be0519ec1cda3431e8e

Fix defects introduced by PR #1255
* Fix use after free of filestream (CID 350856)

Fix use after free in l. 731 of hid_cleanup(), which was identified
as CID 350856 by coverity.

* Fix use after close in get_display_dimensions(...) (CID 350879)

Fix use after close of file stream in l. 242 in get_display_\
dimensions(...), which was identified as CID 350879 by coverity.

* Fix untrusted divisor in get_display_dimensions (CID 350855)

Fix untrusted divisor in l. 249 of get_display_dimensions(...), which
was identified CID 350855 by coverity. Furthermore, guard against
unnecessary big screensizes.

src/plugins/hidsim/hid_injection.cpp
@@ -132,7 +132,9 @@
 #define DRAK_MAGIC_STR 0x4b415244
 #define HEADER_LEN 0xC
 
+/* Constants for determining screensize */
 #define PPM_MAGIC_NUMBER "P6"
+#define SCREEN_MAX_8K 7680
 
 /* Length of the QMP command buffer */
 #define CMD_BUF_LEN 0x400
@@ -207,39 +209,47 @@ static int get_display_dimensions(qmp_connection* qc, dimensions* dims)
         return -1;
 
     /* Extracts display size from .ppm-file created by screendump */
-    FILE* fr = fopen(tmp_path, "r");
+    FILE* f = fopen(tmp_path, "r");
     char ppm_hdr[3];
     int matches = 0;
-    int ret = 0;
 
     /* Reads first three bytes of the potential portable pixmap */
-    matches = fscanf(fr, "%2s", ppm_hdr);
+    matches = fscanf(f, "%2s", ppm_hdr);
 
     /* Checks PPM-header */
     if (matches != 1 || strcmp(ppm_hdr, PPM_MAGIC_NUMBER) != 0)
     {
         fprintf(stderr, "[HIDSIM]: Error reading .ppm-files's magic number\n");
-        fclose(fr);
-        ret = -1;
+        fclose(f);
+        return -1;
     }
 
     /* Retrieves screen dimensions by reading screen dump  */
     uint32_t width = 0, height = 0;
-    matches = fscanf(fr, "%" SCNd32 "%" SCNd32, &width, &height);
-
-    if (matches != 2 )
-    {
-        fprintf(stderr, "[HIDSIM]: Error reading .ppm-files metadata\n");
-        fclose(fr);
-        ret = -1;
-    }
-
-    /* Sanitizes read values; no need for a screen size larger than 65k */
-    dims->width = (uint16_t)(width > UINT16_MAX ? UINT16_MAX : width);
-    dims->height = (uint16_t)(height > UINT16_MAX ? UINT16_MAX : height);
+    matches = fscanf(f, "%" SCNd32 "%" SCNd32, &width, &height);
 
     /* Clean up */
-    fclose(fr);
+    fclose(f);
+
+    if (matches != 2)
+    {
+        fprintf(stderr, "[HIDSIM]: Error reading .ppm-files metadata\n");
+        return -1;
+    }
+
+    /*
+     * Sanitize read values by guarding against a potential division by zero
+     * and checking for utopian big screen sizes.
+     */
+    if (width == 0 || height == 0 ||
+        width > SCREEN_MAX_8K || height > SCREEN_MAX_8K)
+    {
+        fprintf(stderr, "[HIDSIM]: Unsupported screensize\n");
+        return -1;
+    }
+
+    dims->width = (uint16_t) width;
+    dims->height = (uint16_t) height;
 
     /* Removes temp file generated by the screendump-command */
     if (remove(tmp_path) != 0)
@@ -252,7 +262,7 @@ static int get_display_dimensions(qmp_connection* qc, dimensions* dims)
     PRINT_DEBUG("[HIDSIM] Screen dimension: %d x %d\n",
         dims->width, dims->height);
 
-    return ret;
+    return 0;
 }
 
 /* Parses header of HID-template-file */
@@ -728,7 +738,7 @@ static int hid_cleanup(qmp_connection* qc, int fd, FILE* f)
                 qc->sa.sun_path);
     if (f)
         if ((ret = fclose(f)) != 0)
-            fprintf(stderr, "[HIDSIM] Error closing %p", f);
+            fprintf(stderr, "[HIDSIM] Error closing stream\n");
     if (fd >= 0)
         if ((ret = close(fd)) != 0)
             fprintf(stderr, "[HIDSIM] Error closing %d\n", fd);