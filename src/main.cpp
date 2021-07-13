tklengyel/drakvuf

commit 851bcb8fa23881b911a7f293a18361be525fcb8b

Drakvuf write-file option (#1235)
* Add --write-file command line option

Add --write-file <src> <dst> option to inject file into running VM
with libinjector INJECT_METHOD_WRITE_FILE method.

* Modify --write-file option to allow multiple files copying

* Add filepath check for --write-file option

* Fix drakvuf->inject_cmd parameters for write-file option

src/main.cpp
@@ -114,6 +114,9 @@
 #include <exception>
 #include <memory>
 
+#include <map>
+#include <filesystem>
+
 #include "drakvuf.h"
 #include "exitcodes.h"
 
@@ -183,6 +186,8 @@ static void print_usage()
         "\t -c <current_working_dir>  The current working directory for injected executable\n"
         "\t -m <inject_method>        The injection method: [WIN]  : createproc, shellexec, shellcode, doppelganging\n"
         "\t                                               : [LINUX]: execproc -> execlp(), linuxshellcode \n"
+        "\t --write-file <src> <dst>  [WIN] Copy host file <src> into running VM's path <dst> (writefile injection method)\n"
+        "\t                           Can be used multiple times to copy multiple files\n"
         "\t -f <args for exec>        Additional args for exec() (requires -m execproc)\n"
         "\t -g                        Search required for injection functions in all processes\n"
         "\t -j, --injection-timeout <seconds>\n"
@@ -326,6 +331,7 @@ int main(int argc, char** argv)
     uint64_t limited_traps_ttl = UNLIMITED_TTL;
     char const* inject_file = nullptr;
     char const* inject_cwd = nullptr;
+    std::map<std::filesystem::path, std::filesystem::path> write_files;
     injection_method_t injection_method = INJECT_METHOD_CREATEPROC;
     int injection_timeout = 0;
     bool injection_global_search = false;
@@ -409,6 +415,7 @@ int main(int argc, char** argv)
         opt_ipt_dir,
         opt_ipt_trace_os,
         opt_ipt_trace_user,
+        opt_write_file,
     };
     const option long_opts[] =
     {
@@ -462,6 +469,7 @@ int main(int argc, char** argv)
         {"ipt-dir", required_argument, NULL, opt_ipt_dir},
         {"ipt-trace-os", no_argument, NULL, opt_ipt_trace_os},
         {"ipt-trace-user", no_argument, NULL, opt_ipt_trace_user},
+        {"write-file", required_argument, NULL, opt_write_file},
         {NULL, 0, NULL, 0}
     };
     const char* opts = "r:d:i:I:e:m:t:D:o:vx:a:f:spT:S:Mc:nblgj:k:w:W:hF:C";
@@ -525,6 +533,21 @@ int main(int argc, char** argv)
                     injection_method = INJECT_METHOD_EXECPROC;
                 if (!strncmp(optarg, "linuxshellcode", 14))
                     injection_method = INJECT_METHOD_SHELLCODE_LINUX;
+                break;
+            case opt_write_file:
+                if (optind >= argc || *argv[optind] == '-')
+                {
+                    fprintf(stderr, "--write-file <dst> parameter is missing!\n");
+                    return drakvuf_exit_code_t::FAIL;
+                }
+
+                if (!std::filesystem::exists(optarg))
+                {
+                    fprintf(stderr, "--write-file <src> file (%s) not found!\n", optarg);
+                    return drakvuf_exit_code_t::FAIL;
+                }
+
+                write_files[optarg] = argv[optind++];
                 break;
 #ifdef ENABLE_DOPPELGANGING
             case 'B':
@@ -791,6 +814,21 @@ int main(int argc, char** argv)
     sigaction(SIGALRM, &act_timer, nullptr);
 
     vmi_pid_t injected_pid = 0;
+
+    for (const auto&[src, dst] : write_files)
+    {
+        PRINT_DEBUG("Writing file ('%s' -> '%s') into running VM\n", src.c_str(), dst.c_str());
+
+        injector_status_t ret = drakvuf->inject_cmd(injection_pid, injection_thread, dst.c_str(), nullptr, INJECT_METHOD_WRITE_FILE, output, src.c_str(), nullptr, 0, true, 0, nullptr, &injected_pid);
+        if (ret != INJECTOR_SUCCEEDED)
+        {
+            fprintf(stderr, "Failed to copy file (%s) into VM!\n", src.c_str());
+            return drakvuf_exit_code_t::WRITE_FILE_ERROR;
+        }
+
+        drakvuf->interrupt(0); // clear
+    }
+
     if (injection_pid > 0 && inject_file)
     {
         PRINT_DEBUG("Starting injection with PID %i(%i) for %s\n", injection_pid, injection_thread, inject_file);

src/exitcodes.h
@@ -113,5 +113,6 @@ enum drakvuf_exit_code_t
     INJECTION_UNSUCCESSFUL = 4, /* Injection has been done correctly, but
                                  * the sample could not be started
                                  * (corrupted, arch mismatch and so on) */
+    WRITE_FILE_ERROR = 5,
     KERNEL_PANIC = 10,
 };

commit c3edb8c43ef514a8de66ba6dd90204eda3f296d6

Use std::filesystem helper instead of direct include
Ubuntu 18.04 ships with a compiler that exposes std::filesystem as an
experimental feature.
Use local helper file that handles that quirk.

Fixes: 851bcb8 Drakvuf write-file option (#1235)

src/main.cpp
@@ -115,8 +115,8 @@
 #include <memory>
 
 #include <map>
-#include <filesystem>
 
+#include "plugins/filesystem.hpp"
 #include "drakvuf.h"
 #include "exitcodes.h"
 