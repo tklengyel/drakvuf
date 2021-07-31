5c9f18967fe4abf860d48b2f2fc36bff7fb2dd73
Adam Kliś <root@bonusplay.pl>
2021-07-31 13:18:50 +0200

    add missing implementations

--- src/plugins/plugins_ex.h
+++ src/plugins/plugins_ex.h
@@ -457,6 +457,14 @@ public:
     [[nodiscard]]
     std::unique_ptr<libhook::SyscallHook> createSyscallHook(const std::string& syscall_name, Callback cb, int ttl);
 
+    template<typename Params = PluginResult>
+    [[nodiscard]]
+    std::unique_ptr<libhook::Cr3Hook> createCr3Hook(hook_cb_t cb);
+
+    template<typename Params = PluginResult>
+    [[nodiscard]]
+    std::unique_ptr<libhook::Cr3Hook> createCr3Hook(hook_cb_t cb, int ttl);
+
     template<typename Params = PluginResult, typename Callback>
     [[nodiscard]]
     std::unique_ptr<libhook::Cr3Hook> createCr3Hook(Callback cb);
@@ -622,6 +630,24 @@ std::unique_ptr<libhook::SyscallHook> pluginex::createSyscallHook(const std::str
     {
         return std::invoke(cb, static_cast<typename class_type<Callback>::type*>(this), args...);
     }, ttl);
+    if (hook)
+        static_cast<Params*>(hook->trap_->data)->plugin_ = this;
+    else
+        PRINT_DEBUG("[WARNING] libhook failed to setup a trap, returning nullptr!\n");
+    return hook;
+}
+
+template<typename Params>
+std::unique_ptr<libhook::Cr3Hook> pluginex::createCr3Hook(hook_cb_t cb)
+{
+    return createCr3Hook(cb, drakvuf_get_limited_traps_ttl(this->drakvuf));
+}
+
+template<typename Params>
+std::unique_ptr<libhook::Cr3Hook> pluginex::createCr3Hook(hook_cb_t cb, int ttl)
+{
+    static_assert(std::is_base_of_v<PluginResult, Params>, "Params must derive from PluginResult");
+    auto hook = libhook::Cr3Hook::create<Params>(this->drakvuf, cb, ttl);
     if (hook)
         static_cast<Params*>(hook->trap_->data)->plugin_ = this;
     else
