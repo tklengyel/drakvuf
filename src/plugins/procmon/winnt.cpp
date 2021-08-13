dd68e250db398f5c3b2f92397a3379ad769a8e0b
Sergey Kovalev <skovalev@ptsecurity.com>
2021-08-13 11:16:49 +0300

    procmon: fix bits handling error

--- src/plugins/procmon/winnt.cpp
+++ src/plugins/procmon/winnt.cpp
@@ -118,7 +118,7 @@ std::string stringify_protection_attributes(uint32_t attributes, char sep)
     int num = 0;
     for (size_t i = 0; i < 8 * sizeof(attributes) && attributes; ++i)
     {
-        uint32_t attr = VMI_GET_BIT(attributes, i);
+        uint32_t attr = attributes & (1UL << i);
         if (attr == 0)
             continue;
 
