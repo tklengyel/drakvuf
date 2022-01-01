ae2e37b1ced3b667017448022971844315ca310b
Manorit Chawdhry <manorit2001@gmail.com>
2022-01-01 11:13:25 +0530

    happy new year

--- src/libinjector/win/method_helpers.c
+++ src/libinjector/win/method_helpers.c
@@ -1,6 +1,6 @@
 /*********************IMPORTANT DRAKVUF LICENSE TERMS**********************
  *                                                                         *
- * DRAKVUF (C) 2014-2021 Tamas K Lengyel.                                  *
+ * DRAKVUF (C) 2014-2022 Tamas K Lengyel.                                  *
  * Tamas K Lengyel is hereinafter referred to as the author.               *
  * This program is free software; you may redistribute and/or modify it    *
  * under the terms of the GNU General Public License as published by the   *
