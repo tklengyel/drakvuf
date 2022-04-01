6e44c84de73dd72d984559ef00a79eb38ab37d7d
Tamas K Lengyel <tamas@tklengyel.com>
2022-04-01 11:10:49 -0400

    Add basic scaffolding for Meson build system (#1411)
    * Add basic scaffolding for Meson build system
    
    * Add meson compiler check

--- src/libdrakvuf/linux.c
+++ src/libdrakvuf/linux.c
@@ -102,7 +102,6 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <config.h>
 #include <stdlib.h>
 #include <sys/prctl.h>
 #include <string.h>
