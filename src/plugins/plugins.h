f4c617dfae4cf6c8815b4015e408bacce5de48ba
Alexey <30408771+delvinru@users.noreply.github.com>
2022-09-23 19:06:09 +0300

    linux: new plugin ebpfmon (#1517)

--- src/plugins/plugins.h
+++ src/plugins/plugins.h
@@ -229,6 +229,7 @@ typedef enum drakvuf_plugin
     PLUGIN_CALLBACKMON,
     PLUGIN_HIDEVM,
     PLUGIN_PTRACEMON,
+    PLUGIN_EBPFMON,
     __DRAKVUF_PLUGIN_LIST_MAX
 } drakvuf_plugin_t;
 
@@ -272,6 +273,7 @@ static const char* drakvuf_plugin_names[] =
     [PLUGIN_CALLBACKMON] = "callbackmon",
     [PLUGIN_HIDEVM] = "hidevm",
     [PLUGIN_PTRACEMON] = "ptracemon",
+    [PLUGIN_EBPFMON] = "ebpfmon",
 };
 
 static const bool drakvuf_plugin_os_support[__DRAKVUF_PLUGIN_LIST_MAX][VMI_OS_WINDOWS+1] =
@@ -314,6 +316,7 @@ static const bool drakvuf_plugin_os_support[__DRAKVUF_PLUGIN_LIST_MAX][VMI_OS_WI
     [PLUGIN_CALLBACKMON]  = { [VMI_OS_WINDOWS] = 1, [VMI_OS_LINUX] = 0 },
     [PLUGIN_HIDEVM]       = { [VMI_OS_WINDOWS] = 1, [VMI_OS_LINUX] = 0 },
     [PLUGIN_PTRACEMON]    = { [VMI_OS_WINDOWS] = 0, [VMI_OS_LINUX] = 1 },
+    [PLUGIN_EBPFMON]      = { [VMI_OS_WINDOWS] = 0, [VMI_OS_LINUX] = 1 },
 };
 
 class plugin
