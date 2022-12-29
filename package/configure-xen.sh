859854e9d9d553fdd72b8f0b5c87aab210bdc23b
Tamas K Lengyel <tamas@tklengyel.com>
2022-12-29 18:56:33 -0500

    Use gcc-9 building Xen tools on Jammy (#1576)

--- package/configure-xen.sh
+++ package/configure-xen.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Building OVMF may be broken on some distributions
-ovmf="--enable-ovmf"
+# Building OVMF is broken with gcc-11 (default on jammy)
+# Unfortunately we can't just set CC in the environment because the edk2 build system is insane
 
-ubuntu=$(cat /etc/os-release | grep UBUNTU_CODENAME | awk -F'=' '{ print $2 }')
-if [ $ubuntu == "jammy" ]; then
-    ovmf=""
+GCC_VERSION=$(gcc --version | head -n1 | awk '{ print $4 }' | awk -F'.' '{ print $1 }')
+if [ ${GCC_VERSION} -ne 9 ]; then
+    rm /usr/bin/gcc
+    ln -s /usr/bin/gcc-9 /usr/bin/gcc
 fi
 
-./configure --prefix=/usr --enable-githttp --disable-pvshim --enable-systemd ${ovmf}
+./configure --prefix=/usr --enable-githttp --disable-pvshim --enable-systemd --enable-ovmf
 
 exit 0
