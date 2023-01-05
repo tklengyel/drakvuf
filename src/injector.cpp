df2d274dfe349bbdacdb121229707f6c91449b38
Manorit Chawdhry <40723245+manorit2001@users.noreply.github.com>
2023-01-05 19:34:03 +0530

    happy new year 2023 (#1582)

--- src/injector.cpp
+++ src/injector.cpp
@@ -1,6 +1,6 @@
 /*********************IMPORTANT DRAKVUF LICENSE TERMS***********************
  *                                                                         *
- * DRAKVUF (C) 2014-2022 Tamas K Lengyel.                                  *
+ * DRAKVUF (C) 2014-2023 Tamas K Lengyel.                                  *
  * Tamas K Lengyel is hereinafter referred to as the author.               *
  * This program is free software; you may redistribute and/or modify it    *
  * under the terms of the GNU General Public License as published by the   *
@@ -220,7 +220,7 @@ int main(int argc, char** argv)
     vmi_pid_t injected_pid = 0;
     int timeout = 0;
 
-    fprintf(stderr, "%s %s v%s Copyright (C) 2014-2022 Tamas K Lengyel\n",
+    fprintf(stderr, "%s %s v%s Copyright (C) 2014-2023 Tamas K Lengyel\n",
         PACKAGE_NAME, argv[0], PACKAGE_VERSION);
 
     if (argc < 4)
