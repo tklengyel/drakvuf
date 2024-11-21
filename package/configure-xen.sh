6cbc21de3b9dba532ea10a6ed7f7b0e6f3e362b9
Tamas K Lengyel <tamas.lengyel@oracle.com>
2024-11-21 18:45:52 -0500

    Update configure-xen.sh
    Disable OVMF build due to github actions breakage: fatal: could not read Username for 'https://github.com': No such device or address

--- package/configure-xen.sh
+++ package/configure-xen.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+## Disabling OVMF build due to github actions breakage:
+## fatal: could not read Username for 'https://github.com': No such device or address
+
 # Building OVMF is broken with gcc-11 (default on jammy)
 # Unfortunately we can't just set CC in the environment because the edk2 build system is insane
 # gcc-9 is not available on bookworm so there we build with no ovmf at all
 
 OVMF=""
-GCC_VERSION=$(gcc --version | head -n1 | awk '{ print $4 }' | awk -F'.' '{ print $1 }')
-if [ ${GCC_VERSION} -gt 9 ] && [ -f /usr/bin/gcc-9 ]; then
-    rm /usr/bin/gcc
-    ln -s /usr/bin/gcc-9 /usr/bin/gcc
-    OVMF="--enable-ovmf"
-fi
+#GCC_VERSION=$(gcc --version | head -n1 | awk '{ print $4 }' | awk -F'.' '{ print $1 }')
+#if [ ${GCC_VERSION} -gt 9 ] && [ -f /usr/bin/gcc-9 ]; then
+#    rm /usr/bin/gcc
+#    ln -s /usr/bin/gcc-9 /usr/bin/gcc
+#    OVMF="--enable-ovmf"
+#fi
 
 ./configure --prefix=/usr --enable-githttp \
     --disable-pvshim --disable-werror \
