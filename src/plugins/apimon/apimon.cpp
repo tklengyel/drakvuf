3689dd1dacf58fac8ab8c81dbd7903ca173bdc0e
psrok1 <pawel.srokosz@cert.pl>
2025-07-21 21:54:19 +0200

    Fix: changed make_hook_id to not overwrite return hook when apicall was nested

--- src/plugins/apimon/apimon.cpp
+++ src/plugins/apimon/apimon.cpp
@@ -123,11 +123,17 @@ struct ApimonReturnHookData : PluginResult
 
 }
 
-static uint64_t make_hook_id(const drakvuf_trap_info_t* info)
+static uint64_t make_hook_id(const drakvuf_trap_info_t* info, addr_t target_rsp)
 {
-    uint64_t u64_pid = info->attached_proc_data.pid;
-    uint64_t u64_tid = info->attached_proc_data.tid;
-    return (u64_pid << 32) | u64_tid;
+    /*
+    This function doesn't have to be exact. We're taking here the low 18-bit part of PID/TID
+    (without 2 LSB that are always zeroed on Windows) and low 32-bit part of target_rsp
+    which identifies the stack pointer at the time when pre-hook was fired.
+    */
+    uint64_t pid_part = (info->attached_proc_data.pid >> 2) & 0xFFFF;
+    uint64_t tid_part = (info->attached_proc_data.tid >> 2) & 0xFFFF;
+    uint64_t u64_target_rsp = static_cast<uint64_t>(target_rsp) & 0xFFFFFFFF;
+    return (u64_target_rsp << 32) | (pid_part << 16) | (tid_part);
 }
 
 static event_response_t delete_process_cb(drakvuf_t drakvuf, drakvuf_trap_info_t* info)
@@ -203,7 +209,7 @@ event_response_t apimon::usermode_return_hook_cb(drakvuf_t drakvuf, drakvuf_trap
 
     usermode_print(info, params->arguments, params->target);
 
-    uint64_t hookID = make_hook_id(info);
+    uint64_t hookID = make_hook_id(info, params->target_rsp);
     ret_hooks.erase(hookID);
 
     return VMI_EVENT_RESPONSE_NONE;
@@ -251,10 +257,10 @@ static event_response_t usermode_hook_cb(drakvuf_t drakvuf, drakvuf_trap_info* i
     }
     else
     {
-        uint64_t hookID = make_hook_id(info);
         auto hook = plugin->createReturnHook<ApimonReturnHookData>(info,
                 &apimon::usermode_return_hook_cb, target->target_name.data(), drakvuf_get_limited_traps_ttl(drakvuf));
         auto params = libhook::GetTrapParams<ApimonReturnHookData>(hook->trap_);
+        uint64_t hookID = make_hook_id(info, params->target_rsp);
 
         params->arguments = std::move(arguments);
         params->target = target;
