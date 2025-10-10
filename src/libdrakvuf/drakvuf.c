5e83aa51a5faa6346f64eb1ad8ffddfea5152219
Logan Freijo <5163050+lfreijo@users.noreply.github.com>
2025-10-10 09:47:52 -0400

    Fix fileextractor and other plugins crashing
    Removed g_assert for drakvuf_lookup_injection which can cause fileextractor and other code to crash processing when trying to unhook the injection.

--- src/libdrakvuf/drakvuf.c
+++ src/libdrakvuf/drakvuf.c
@@ -1281,8 +1281,8 @@ void drakvuf_remove_injection(drakvuf_t drakvuf, drakvuf_trap_info_t* info)
 
     drakvuf_lock_and_get_vmi(drakvuf);
 
-    g_assert (drakvuf_lookup_injection(drakvuf, info) == info->trap);
-    g_assert (g_hash_table_remove(drakvuf->injections_in_progress, key));
+    if (drakvuf_lookup_injection(drakvuf, info) == info->trap)
+        g_assert (g_hash_table_remove(drakvuf->injections_in_progress, key));
 
     drakvuf_release_vmi(drakvuf);
 }
